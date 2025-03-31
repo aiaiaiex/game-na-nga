ddbfbfc5f8fdd2e588bf2bb293e599d872f2c18b
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-03-31 19:36:23 +0800

    Configure @svgr/plugin-svgo

--- game-na-nga-client/vite.config.ts
+++ game-na-nga-client/vite.config.ts
@@ -5,5 +5,38 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+
+        svgoConfig: {
+          plugins: [
+            {
+              name: "preset-default",
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                  cleanupIds: {
+                    minify: false,
+                  },
+                },
+              },
+            },
+            "removeXMLNS",
+            "convertStyleToAttrs",
+            "removeDimensions",
+          ],
+        },
+      },
+
+      esbuildOptions: {},
+
+      include: "**/*.svg?react",
+
+      exclude: "",
+    }),
+  ],
 });
