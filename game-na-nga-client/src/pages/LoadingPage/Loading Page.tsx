2809523891fe5516a72b4d5c371cbc48be23a812
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-04-02 22:19:40 +0800

    Change height

--- game-na-nga-client/src/pages/LoadingPage/Loading Page.tsx
+++ game-na-nga-client/src/pages/LoadingPage/Loading Page.tsx
@@ -7,7 +7,7 @@ interface LoadingPageProps extends ComponentPropsWithoutRef<"div"> {}
 export function LoadingPage({ ...attributes }: LoadingPageProps): JSX.Element {
   return (
     <div
-      className="bg-gnn-white flex min-h-screen w-screen flex-col items-center justify-center"
+      className="bg-gnn-white flex h-screen min-h-screen w-screen flex-col items-center justify-center"
       {...attributes}
     >
       <LoadingIcon className="fill-gnn-red max-h-20 min-h-20 max-w-20 min-w-20 animate-pulse" />
