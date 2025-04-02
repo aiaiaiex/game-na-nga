080146024189c9dcd1a04580aaa490c3c665dbc6
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-04-02 22:18:34 +0800

    Design page

--- game-na-nga-client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ game-na-nga-client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,6 +1,8 @@
 import { ComponentPropsWithoutRef, JSX, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
+import LoadingIcon from "../../assets/svgs/puzzle-piece.svg?react";
+
 interface NotFoundPageProps extends ComponentPropsWithoutRef<"div"> {}
 
 export function NotFoundPage({
@@ -22,12 +24,23 @@ export function NotFoundPage({
   }, [remainingSeconds, navigate]);
 
   return (
-    <div {...attributes}>
-      <span className="flex">PAGE NOT FOUND</span>
-      <span className="flex">REDIRECTING YOU IN</span>
-      <span className="flex">
-        {remainingSeconds} {remainingSeconds !== 1 ? "SECONDS" : "SECOND"}
+    <div
+      className="bg-gnn-white justify-top flex h-screen min-h-screen w-screen flex-col items-center"
+      {...attributes}
+    >
+      <span className="font-jersey-25 text-gnn-red px-4 text-center text-5xl">
+        GAME NA NGA
       </span>
+      <div className="flex h-full flex-col items-center justify-center gap-y-4">
+        <span className="font-jersey-25 text-center text-5xl">
+          <span className="block">PAGE NOT FOUND</span>
+          <span className="block">REDIRECTING YOU IN</span>
+          <span className="block">
+            {remainingSeconds} {remainingSeconds !== 1 ? "SECONDS" : "SECOND"}
+          </span>
+        </span>
+        <LoadingIcon className="fill-gnn-red max-h-20 min-h-20 max-w-20 min-w-20 animate-pulse" />
+      </div>
     </div>
   );
 }
