c0637d34a4bcd729ea45e7ec4481f54669780876
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-04-02 15:39:46 +0800

    Redirect to home page

--- game-na-nga-client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ game-na-nga-client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,33 @@
-import { ComponentPropsWithoutRef, JSX } from "react";
+import { ComponentPropsWithoutRef, JSX, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 interface NotFoundPageProps extends ComponentPropsWithoutRef<"div"> {}
 
 export function NotFoundPage({
   ...attributes
 }: NotFoundPageProps): JSX.Element {
+  const [remainingSeconds, setRemainingSeconds] = useState<number>(5);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (remainingSeconds > 1) {
+        setRemainingSeconds(remainingSeconds - 1);
+      } else {
+        setRemainingSeconds(0);
+        navigate("/");
+      }
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [remainingSeconds, navigate]);
+
   return (
     <div {...attributes}>
-      <span>Not Found Page</span>
+      <span className="flex">PAGE NOT FOUND</span>
+      <span className="flex">REDIRECTING TO HOME PAGE IN</span>
+      <span className="flex">
+        {remainingSeconds} {remainingSeconds !== 1 ? "SECONDS" : "SECOND"}
+      </span>
     </div>
   );
 }
