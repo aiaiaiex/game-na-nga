84c03d46f84057958347bcca0a55ed1defda2c88
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-04-02 23:54:55 +0800

    Design page

--- game-na-nga-client/src/pages/LoginPage/LoginPage.tsx
+++ game-na-nga-client/src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,8 @@
-import axios from "axios";
 import { ComponentPropsWithoutRef, JSX, useState } from "react";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
+import axios from "axios";
+
+import LoadingIcon from "../../assets/svgs/puzzle-piece.svg?react";
 
 interface LogInPageProps extends ComponentPropsWithoutRef<"div"> {}
 
@@ -33,54 +35,58 @@ export function LogInPage({ ...attributes }: LogInPageProps): JSX.Element {
   };
 
   return (
-    <div {...attributes}>
-      <div>
-        <span>Login</span>
-      </div>
-      <form>
-        <div className="flex flex-col">
-          <label htmlFor="email">Email</label>
+    <div
+      {...attributes}
+      className="bg-gnn-white flex h-screen min-h-screen w-screen flex-col items-center justify-center gap-y-4"
+    >
+      <LoadingIcon className="fill-gnn-red -mt-20 max-h-20 min-h-20 max-w-20 min-w-20 animate-bounce" />
+      <div className="flex h-fit w-96 flex-col items-center justify-start rounded-2xl bg-white p-4 shadow">
+        <span className="font-jersey-10 text-gnn-red text-5xl">
+          GAME NA NGA
+        </span>
+        <span className="font-jersey-25 text-lg">Log in to continue.</span>
+        <form className="mt-2 flex w-full flex-col items-center justify-start gap-y-2">
           <input
-            className="rounded-2xl shadow"
+            className="border-gnn-gray font-jersey-25 block w-full rounded border p-1 text-xl shadow"
             id="email"
             type="email"
             autoComplete="email"
-            placeholder="Insert email here..."
+            placeholder="Email"
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
             }}
           />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="password">Password</label>
           <input
-            className="rounded-2xl shadow"
+            className="border-gnn-gray font-jersey-25 block w-full rounded border p-1 text-xl shadow"
             id="password"
             type="password"
             autoComplete="current-password"
-            placeholder="Insert password here..."
+            placeholder="Password"
             value={password}
             onChange={(e) => {
               setPassword(e.target.value);
             }}
           />
-        </div>
-        <div>
           <button
-            className="cursor-pointer rounded-2xl border shadow"
+            className="bg-gnn-red font-jersey-25 w-full cursor-pointer rounded p-1 shadow"
             type="button"
             onClick={login}
           >
-            Login
+            <span className="text-xl font-bold text-white">Log In</span>
           </button>
-        </div>
-      </form>
-      {errorMessage && (
-        <div>
-          <span>{errorMessage}</span>
-        </div>
-      )}
+        </form>
+      </div>
+      <Link to="/signup">
+        <span className="text-gnn-red font-jersey-25 text-lg hover:underline">
+          No account yet? Sign up.
+        </span>
+      </Link>
+      <span
+        className={`font-jersey-25 text-lg ${errorMessage ? "" : "invisible"}`}
+      >
+        {errorMessage || "hidden"}
+      </span>
     </div>
   );
 }
