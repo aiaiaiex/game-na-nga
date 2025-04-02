002f810ca8cb9381f51c4e99b49f9fd66ab14ae7
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-04-02 21:53:17 +0800

    Design loader

--- game-na-nga-client/src/pages/LoadingPage/Loading Page.tsx
+++ game-na-nga-client/src/pages/LoadingPage/Loading Page.tsx
@@ -1,11 +1,16 @@
 import { ComponentPropsWithoutRef, JSX } from "react";
 
+import LoadingIcon from "../../assets/svgs/puzzle-piece.svg?react";
+
 interface LoadingPageProps extends ComponentPropsWithoutRef<"div"> {}
 
 export function LoadingPage({ ...attributes }: LoadingPageProps): JSX.Element {
   return (
-    <div {...attributes}>
-      <span>Loading Page</span>
+    <div
+      className="bg-gnn-white flex min-h-screen w-screen flex-col items-center justify-center"
+      {...attributes}
+    >
+      <LoadingIcon className="fill-gnn-red max-h-16 min-h-16 max-w-16 min-w-16 animate-pulse" />
     </div>
   );
 }
